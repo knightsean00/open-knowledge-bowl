9c78a44bf329e8da30bbc3a361a344f3c568a3c0
Sean Knight <seknight@mit.edu>
2025-05-23 17:28:31 -0400

    Add support for sounds and custom files

--- question-ui/src/QuestionPage.tsx
+++ question-ui/src/QuestionPage.tsx
@@ -1,16 +1,16 @@
-import { QuestionArray, SupplementType } from './Types';
-import { useState, useEffect } from 'react';
-import useSound from 'use-sound';
+import { QuestionArray } from "./Types";
+import { useState } from "react";
+import "./progress.scss";
+import AudioPlayer from "react-h5-audio-player";
 
 interface QuestionPageProps {
-  teamScores: number[],
-  setTeamScores: React.Dispatch<React.SetStateAction<number[]>>
-  selectQuestions: QuestionArray,
-  setQuestions: React.Dispatch<React.SetStateAction<QuestionArray>>,
-  showAnswers: boolean,
-  setShowAnswers: React.Dispatch<React.SetStateAction<boolean>>,
+  teamScores: number[];
+  setTeamScores: React.Dispatch<React.SetStateAction<number[]>>;
+  selectQuestions: QuestionArray;
+  setQuestions: React.Dispatch<React.SetStateAction<QuestionArray>>;
+  showAnswers: boolean;
+  setShowAnswers: React.Dispatch<React.SetStateAction<boolean>>;
 }
-
 
 const QuestionPage: React.FC<QuestionPageProps> = ({
   teamScores,
@@ -21,32 +21,14 @@ const QuestionPage: React.FC<QuestionPageProps> = ({
   setShowAnswers,
 }) => {
   const [questionNumber, setQuestionNumber] = useState(1);
-  const [soundUrl, setSoundUrl] = useState("https://archive.org/download/78_merry-christmas_korla-pandit-jette-satin-i-berlin_gbia0340413/03%20-%20AVA%20MARIA%20-%20KORLA%20PANDIT%20-%20Schubert.mp3");
-  const [play, { stop }] = useSound(soundUrl);
 
   const moveToNextQuestion = () => {
     setQuestions((old) => {
-      const toReturn = old.slice(1);
-      if (toReturn[0].Supplement != undefined && toReturn[0].Supplement.Type == SupplementType.SOUND) {
-        console.log("setting sound to", toReturn[0].Supplement.Data);
-        setSoundUrl(toReturn[0].Supplement.Data);
-      }
-      return toReturn;
+      return old.slice(1);
     });
 
     setQuestionNumber((old) => old + 1);
-  }
-
-  useEffect(() => {
-    console.log("in sound url")
-    if (soundUrl.length > 0) {
-      console.log("playing")
-      play();
-    } else {
-      console.log("stopping")
-      stop();
-    }
-  }, [soundUrl]);
+  };
 
   if (selectQuestions.length === 0) {
     const indexedArray = teamScores.map((value, idx) => ({ value, idx }));
@@ -57,33 +39,59 @@ const QuestionPage: React.FC<QuestionPageProps> = ({
     });
 
     return (
-      <div style={{ display: "flex", flexDirection: "column", marginTop: "3rem", alignItems: "center", wordBreak: "normal" }}>
-        {
-          rankingArray.map((scoreIdx, idx) => {
-            if (idx === 0) {
-              return (
-                <h1 key={idx}>Rank {idx + 1} - {teamScores[scoreIdx]} Questions Correct - Team {scoreIdx + 1} </h1>
-              );
-            }
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "3rem",
+          alignItems: "center",
+          wordBreak: "normal",
+        }}
+      >
+        {rankingArray.map((scoreIdx, idx) => {
+          if (idx === 0) {
             return (
-              <h2 key={idx}>Rank {idx + 1} - {teamScores[scoreIdx]} Questions Correct - Team {scoreIdx + 1} </h2>
+              <h1 key={idx}>
+                Rank {idx + 1} - {teamScores[scoreIdx]} Questions Correct - Team{" "}
+                {scoreIdx + 1}{" "}
+              </h1>
             );
-          })
-        }
+          }
+          return (
+            <h2 key={idx}>
+              Rank {idx + 1} - {teamScores[scoreIdx]} Questions Correct - Team{" "}
+              {scoreIdx + 1}{" "}
+            </h2>
+          );
+        })}
       </div>
-    )
+    );
   }
 
-  let questionSupplement = (<></>);
-  if (selectQuestions[0].Supplement != undefined) {
-    switch (selectQuestions[0].Supplement.Type) {
-      case SupplementType.PICTURE:
-        questionSupplement = (<img src={selectQuestions[0].Supplement.Data} />);
+  let questionSupplement = <></>;
+
+  if (selectQuestions[0].supplement != undefined) {
+    switch (selectQuestions[0].supplement.type) {
+      case "PICTURE":
+        questionSupplement = <img src={selectQuestions[0].supplement.data} />;
         break;
-      case SupplementType.EMBED:
-        questionSupplement = (<iframe height="675px" width="1200px" src={selectQuestions[0].Supplement.Data} style={{ border: "none" }} allow="autoplay" />);
+      case "EMBED":
+        questionSupplement = (
+          <iframe
+            height="675px"
+            width="1200px"
+            src={selectQuestions[0].supplement.data}
+            style={{ border: "none" }}
+            allow="autoplay"
+          />
+        );
         break;
-      case SupplementType.SOUND:
+      case "SOUND":
+        questionSupplement = (
+          <div style={{ width: "100%", marginBottom: "3rem" }}>
+            <AudioPlayer src={selectQuestions[0].supplement.data} />
+          </div>
+        );
         break;
       default:
         break;
@@ -91,50 +99,84 @@ const QuestionPage: React.FC<QuestionPageProps> = ({
   }
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", marginTop: "3rem", alignItems: "center" }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        marginTop: "3rem",
+        alignItems: "center",
+      }}
+    >
       <div className="row">
         <h3>Question #{questionNumber}</h3>
       </div>
       <div className="row">
         <h3>{selectQuestions.length} Remaining Questions</h3>
       </div>
-      <div style={{ maxWidth: "1200px", display: "flex", flexDirection: "column", marginTop: "5rem", alignItems: "center", wordBreak: "normal" }}>
-        <h1>{selectQuestions[0].Question}</h1>
+      <div
+        style={{
+          maxWidth: "1200px",
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "5rem",
+          alignItems: "center",
+          wordBreak: "normal",
+        }}
+      >
+        <h1>{selectQuestions[0].question}</h1>
         {questionSupplement}
-        {
-          showAnswers ?
-            <h2 style={{ color: "rgb(149, 92, 175)" }}>{selectQuestions[0].Answer}</h2> :
-            <></>
-        }
+        {showAnswers ? (
+          <h2 style={{ color: "rgb(149, 92, 175)" }}>
+            {selectQuestions[0].answer}
+          </h2>
+        ) : (
+          <></>
+        )}
       </div>
       <div className="row">
-        <button className="big" style={{ marginTop: "10rem" }} onClick={moveToNextQuestion}>None Correct</button>
+        <button
+          className="big"
+          style={{ marginTop: "10rem" }}
+          onClick={moveToNextQuestion}
+        >
+          None Correct
+        </button>
       </div>
-      <div className="row" style={{ width: "100%", marginTop: "3rem", flexWrap: "wrap" }}>
-        {
-          teamScores.map((val, idx) => {
-            return (
-              <button className="big" onClick={() => {
+      <div
+        className="row"
+        style={{ width: "100%", marginTop: "3rem", flexWrap: "wrap" }}
+      >
+        {teamScores.map((val, idx) => {
+          return (
+            <button
+              className="big"
+              onClick={() => {
                 setTeamScores((old) => {
                   const toSet = [...old];
                   toSet[idx] = val + 1;
                   return toSet;
                 });
                 moveToNextQuestion();
-              }}>Team {idx + 1} - {val}</button>
-            )
-          })
-        }
+              }}
+            >
+              Team {idx + 1} - {val}
+            </button>
+          );
+        })}
       </div>
       <div className="row" style={{ marginTop: "10rem" }}>
         <label>Show Answers</label>
-        <input type="checkbox" checked={showAnswers} onClick={() => setShowAnswers(!showAnswers)} />
+        <input
+          type="checkbox"
+          checked={showAnswers}
+          onClick={() => setShowAnswers(!showAnswers)}
+        />
       </div>
       <div className="row">
         <button onClick={() => setQuestions([])}>End Game</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default QuestionPage;
--- question-ui/src/Types.tsx
+++ question-ui/src/Types.tsx
@@ -1,22 +1,47 @@
-export enum SupplementType {
-    PICTURE = "PICTURE",
-    SOUND = "SOUND",
-    EMBED = "EMBED",
-}
+import { z } from "zod/v4";
 
-export type QuestionSupplement = {
-    Data: string;
-    Type: SupplementType;
-}
+// export enum SupplementType {
+//   PICTURE = "PICTURE",
+//   SOUND = "SOUND",
+//   EMBED = "EMBED",
+// }
 
-export type Question = {
-    Question: string;
-    Answer: string;
-    Supplement?: QuestionSupplement;
-}
+export const SupplementTypeSchema = z.enum(["PICTURE", "SOUND", "EMBED"]);
+export type SupplementType = z.infer<typeof SupplementTypeSchema>;
 
-export type QuestionArray = Array<Question>
+// export type QuestionSupplement = {
+//   data: string;
+//   type: SupplementType;
+// };
 
-export type QuestionBank = {
-    [key: string]: QuestionArray
-}
+export const QuestionSupplementSchema = z.object({
+  data: z.string(),
+  type: SupplementTypeSchema,
+});
+export type QuestionSupplement = z.infer<typeof QuestionSupplementSchema>;
+
+// export type Question = {
+//   question: string;
+//   answer: string;
+//   supplement?: QuestionSupplement;
+// };
+
+export const QuestionSchema = z.object({
+  question: z.string(),
+  answer: z.string(),
+  supplement: z.optional(QuestionSupplementSchema),
+});
+export type Question = z.infer<typeof QuestionSchema>;
+
+// export type QuestionArray = Array<Question>;
+
+export const QuestionArraySchema = z.array(QuestionSchema);
+export type QuestionArray = z.infer<typeof QuestionArraySchema>;
+
+export const QuestionBankSchema = z.record(z.string(), QuestionArraySchema);
+
+export type QuestionBank = z.infer<typeof QuestionBankSchema>;
+
+// export type QuestionBank = {
+//   [key: string]: QuestionArray;
+// };
